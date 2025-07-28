4c0f04f017e00bd026821c45688d9caef20a92a2
Divyabharathi23012007 <divibharathi2007@gmail.com>
2025-07-28 19:11:30 +0530

    Add files via upload

---
+++ AreaOfRectangle1/src/areaofrectangle1/Area.java
@@ -0,0 +1,8 @@
+package areaofrectangle1;
+
+public class Area{
+	public static void main(String[] args) {
+		int a=3,b=5,c=a*b;
+		System.out.println("The area of rectangle of dimensions "+a+" and "+b+" units is "+c);
+	}
+}
---
+++ AreaOfRectangle1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module AreaOfRectangle1 {
+}
