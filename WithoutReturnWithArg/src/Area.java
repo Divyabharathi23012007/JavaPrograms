ff8c91d20d8121e7dec3ab27eb6ac77d5af9cdb7
Divyabharathi23012007 <divibharathi2007@gmail.com>
2025-07-29 11:30:04 +0530

    Add files via upload

---
+++ WithoutReturnWithArg/src/Area.java
@@ -0,0 +1,11 @@
+
+public class Area {
+    public void area(int a,int b) {
+    	int c=a*b;
+    	System.out.println(c);
+    	}
+    public static void main(String[] args) {
+    	Area o= new Area();
+    	o.area(5,8);
+    }
+}
