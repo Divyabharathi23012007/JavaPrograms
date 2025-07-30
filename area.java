e5dcc5b78c95a15c093f5b19b3e25ac18e7fd8b5
Divyabharathi23012007 <divibharathi2007@gmail.com>
2025-07-30 18:39:43 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,13 @@
+package examples;
+
+//constructor
+public class area {
+	public area(){
+		System.out.println(3*5);
+	}
+
+	public static void main(String[] args) {
+		area o= new area();
+	}
+
+}
