e4b75cd21b9155e5e187e7904bb7e9c2dab84bd2
Divyabharathi23012007 <divibharathi2007@gmail.com>
2025-08-04 19:58:52 +0530

    Add files via upload

---
+++ Two.java
@@ -0,0 +1,12 @@
+package examples;
+
+class One {
+    int a=3;
+}
+public class Two extends One{
+	int b=5;
+	public static void main(String[] args) {
+		Two o= new Two();
+		System.out.println(o.a * o.b);
+	}
+}
