35f1f7add45733cde920a4ada8bd9f5c1366c4ed
Divyabharathi23012007 <divibharathi2007@gmail.com>
2025-08-05 18:51:56 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,25 @@
+//polymorphism 
+
+//Method Over Riding
+
+class rit{
+	public void disp() {
+		System.out.println("RIT");
+	}
+	
+}
+class rec extends rit{
+	public void disp() {
+		System.out.println("REC");
+	}
+}
+
+public class Test{
+	public static void main(String[] args){
+        rit o=new rit();
+            o.disp();
+        rit o1=new rec();
+            o1.disp();
+        
+	}
+}
