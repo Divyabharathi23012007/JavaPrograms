7b9524e81708da0f2f72c6157fc9150e60954661
Divyabharathi23012007 <divibharathi2007@gmail.com>
2025-11-01 19:45:11 +0530

    Add files via upload

---
+++ SwingSliderDemo.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.*;
+
+public class SwingSliderDemo extends Application {
+    public void start(Stage s) {
+        SwingNode sn=new SwingNode();
+        sn.setContent(new JSlider());
+        s.setScene(new Scene(new StackPane(sn),200,100));
+        s.show();
+    }
+    public static void main(String[] a){launch(a);}
+}
